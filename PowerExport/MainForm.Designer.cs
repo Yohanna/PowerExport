10544d2bbb22031bb3cbd9bdf4e3450d3eb6f593
Yohanna <Yohanna@users.noreply.github.com>
2014-04-21 02:07:50 -0700

    Finished Form Load code. Added few things in Change Button.

--- PowerExport/MainForm.Designer.cs
+++ PowerExport/MainForm.Designer.cs
@@ -28,30 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.changeButton = new System.Windows.Forms.Button();
             this.officeVersionComboBox = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.resolutionComboBox = new System.Windows.Forms.ComboBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.errorToolTip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // changeButton
             // 
             this.changeButton.AutoSize = true;
             this.changeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.changeButton.Location = new System.Drawing.Point(159, 185);
+            this.changeButton.Location = new System.Drawing.Point(179, 93);
             this.changeButton.Name = "changeButton";
             this.changeButton.Size = new System.Drawing.Size(133, 39);
             this.changeButton.TabIndex = 26;
@@ -64,12 +56,7 @@
             this.officeVersionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.officeVersionComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.officeVersionComboBox.FormattingEnabled = true;
-            this.officeVersionComboBox.Items.AddRange(new object[] {
-            "2013",
-            "2010",
-            "2007",
-            "2003"});
-            this.officeVersionComboBox.Location = new System.Drawing.Point(424, 134);
+            this.officeVersionComboBox.Location = new System.Drawing.Point(426, 30);
             this.officeVersionComboBox.Name = "officeVersionComboBox";
             this.officeVersionComboBox.Size = new System.Drawing.Size(59, 26);
             this.officeVersionComboBox.TabIndex = 25;
@@ -78,27 +65,17 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(340, 137);
+            this.label4.Location = new System.Drawing.Point(342, 30);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 18);
             this.label4.TabIndex = 24;
             this.label4.Text = "For Office:";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 20);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(136, 18);
-            this.label3.TabIndex = 21;
-            this.label3.Text = "Current Resolution:";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(274, 137);
+            this.label2.Location = new System.Drawing.Point(285, 30);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(27, 18);
             this.label2.TabIndex = 20;
@@ -108,7 +85,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 132);
+            this.label1.Location = new System.Drawing.Point(14, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(191, 18);
             this.label1.TabIndex = 19;
@@ -121,146 +98,38 @@
             this.resolutionComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.resolutionComboBox.FormattingEnabled = true;
             this.resolutionComboBox.Items.AddRange(new object[] {
+            "50",
             "96",
             "100",
             "150",
             "200",
             "250",
             "300"});
-            this.resolutionComboBox.Location = new System.Drawing.Point(209, 132);
+            this.resolutionComboBox.Location = new System.Drawing.Point(211, 30);
             this.resolutionComboBox.Name = "resolutionComboBox";
             this.resolutionComboBox.Size = new System.Drawing.Size(59, 26);
             this.resolutionComboBox.TabIndex = 18;
             // 
-            // tableLayoutPanel1
+            // errorToolTip
             // 
-            this.tableLayoutPanel1.AutoSize = true;
-            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label8, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.label9, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.label10, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label11, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label12, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.label13, 1, 3);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(171, 20);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.37363F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.97802F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(108, 90);
-            this.tableLayoutPanel1.TabIndex = 28;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(3, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(45, 20);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "2013";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(3, 22);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(45, 20);
-            this.label7.TabIndex = 0;
-            this.label7.Text = "2010";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(3, 46);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(45, 20);
-            this.label8.TabIndex = 0;
-            this.label8.Text = "2007";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(3, 66);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(45, 20);
-            this.label9.TabIndex = 0;
-            this.label9.Text = "2003";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(54, 0);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(51, 20);
-            this.label10.TabIndex = 0;
-            this.label10.Text = "label6";
-            // 
-            // label11
-            // 
-            this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(54, 22);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(51, 24);
-            this.label11.TabIndex = 0;
-            this.label11.Text = "label6";
-            // 
-            // label12
-            // 
-            this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(54, 46);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(51, 20);
-            this.label12.TabIndex = 0;
-            this.label12.Text = "label6";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(54, 66);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(51, 20);
-            this.label13.TabIndex = 0;
-            this.label13.Text = "label6";
+            this.errorToolTip.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Error;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 236);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(502, 144);
             this.Controls.Add(this.resolutionComboBox);
             this.Controls.Add(this.changeButton);
             this.Controls.Add(this.officeVersionComboBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Power Export";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -271,19 +140,10 @@
         private System.Windows.Forms.Button changeButton;
         private System.Windows.Forms.ComboBox officeVersionComboBox;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox resolutionComboBox;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.ToolTip errorToolTip;
     }
 }
 
--- PowerExport/MainForm.cs
+++ PowerExport/MainForm.cs
@@ -13,23 +13,57 @@ namespace PowerExport
 {
     public partial class MainForm : Form
     {
+
+        object[,] officeVersion = {
+    {"2013", @"HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\PowerPoint\Options", true},
+    {"2010", @"HKEY_CURRENT_USER\Software\Microsoft\Office\14.0\PowerPoint\Options", true},
+    {"2007", @"HKEY_CURRENT_USER\Software\Microsoft\Office\12.0\PowerPoint\Options", true},
+    {"2003", @"HKEY_CURRENT_USER\Software\Microsoft\Office\11.0\PowerPoint\Options", true}
+    
+    };
+
+
         public MainForm()
         {
             InitializeComponent();
-            resolutionComboBox.SelectedIndex = 0;
-            officeVersionComboBox.SelectedIndex = 0;
-
-        //    RegistryKey key = Registry.CurrentUser.OpenSubKey("test");
-        //    key.SetValue()
-        //Registry.
-
-            
         }
 
         private void changeButton_Click(object sender, EventArgs e)
         {
-            //tAblelAYOutPanel1.
+            string requiredResolution;
 
+            try {
+
+                requiredResolution = resolutionComboBox.SelectedItem.ToString();
+            }
+            catch (NullReferenceException ex) {
+                errorToolTip.Show("Please select the required resolution first!", resolutionComboBox);
+                return;
+            }
+
+            int Version = officeVersionComboBox.SelectedIndex;
+
+            if (Version == -1) {
+                errorToolTip.Show("Please select the office version you wish to change the resolution for!", officeVersionComboBox);
+                return;
+            }
+
+            MessageBox.Show("Changing version: " + Version.ToString() + " at " + officeVersion[Version, 1] + " with " + requiredResolution);
+
+
+            //Registry.SetValue(, 1].ToString(), "ExportBitmapResolution", 200, RegistryValueKind.DWord);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++) {
+                if (Registry.GetValue(officeVersion[i, 1].ToString(), "ExportBitmapResolution", "96") == null) {
+                    officeVersion[i, 2] = false;
+                }
+                else {
+                    officeVersionComboBox.Items.Add(officeVersion[i, 0].ToString());
+                }
+            }
         }
 
 
